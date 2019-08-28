13ce0f646eab061f28daba2b56bf8ee4d72edd30
Lillian Wright <10803934@my.uvu.edu>
2019-08-28 13:42:15 -0600

    Unity Update

---
+++ Basics/Assets/Scripts/TriggerEventBehavior.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehavior : MonoBehaviour
+{
+    public UnityEvent TriggerEnterEvent, TriggerStayEvent;
+
+    private void OnTriggerEvent(Collider other)
+    {
+        TriggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        TriggerStayEvent.Invoke();
+    }
+    
+}
