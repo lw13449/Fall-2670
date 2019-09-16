b56002dd76495224c4f24faee70c887b5bfa47d7
Lillian Wright <10803934@my.uvu.edu>
2019-09-16 09:59:36 -0600

    Code Fix-its

--- Basics/Assets/Scripts/TriggerEventBehavior.cs
+++ Basics/Assets/Scripts/TriggerEventBehavior.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class TriggerEventBehaviour : MonoBehaviour
+public class TriggerEventBehavior : MonoBehaviour
 {
 	public UnityEvent TriggerEnterEvent, TriggerStayEvent;
 	
