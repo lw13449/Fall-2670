31983659d2af8902ca9f9590bd144701ca2b8e5c
Lillian Wright <10803934@my.uvu.edu>
2019-09-16 11:29:05 -0600

    sleeping action trial

---
+++ Basics/Assets/Scripts/Variables.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Variables : MonoBehaviour
+{
+
+    public GameAction sleepfunction;
+    public UnityEvent sleeping;
+
+    private void Start()
+    {
+        sleepfunction.action += Action;
+    }
+
+    
+    private void Action()
+    {
+        sleeping.Invoke();
+    }
+}
