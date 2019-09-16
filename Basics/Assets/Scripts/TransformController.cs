916b2654b1b34c7527845a7f5fb02472dd80fbd6
Lillian Wright <10803934@my.uvu.edu>
2019-09-16 14:28:24 -0600

    TransformController and Prefabs

--- Basics/Assets/Scripts/TransformController.cs
+++ Basics/Assets/Scripts/TransformController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerBooster : MonoBehaviour
+public class TransformController : MonoBehaviour
 {
     public FloatData speed ;
    
