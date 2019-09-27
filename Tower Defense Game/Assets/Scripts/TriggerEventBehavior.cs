7a40604d4398142d5fd806416970d28c45dea8e6
Lillian Wright <10803934@my.uvu.edu>
2019-09-27 09:24:16 -0600

    tower defense project setup

---
+++ Tower Defense Game/Assets/Scripts/TriggerEventBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventBehavior : MonoBehaviour
+{
+
+    public UnityEvent TriggerEnterEvent, TriggerStayEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggerEnterEvent.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        TriggerStayEvent.Invoke();
+    }
+}
