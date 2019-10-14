dff712c61deda91fe2a5dc9796ff7f218be89996
Lillian Wright <10803934@my.uvu.edu>
2019-10-14 14:36:54 -0600

    scriptable object character mover and more

---
+++ Basics/Assets/Scripts/CreateSOData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateSOData : MonoBehaviour
+{
+    public IntData data;
+    private IntData newData;
+    
+    void Start()
+    {
+        newData = Instantiate(data);
+    }
+
+}
---
+++ Basics/Assets/Scripts/System Components/Collections.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Collections : ScriptableObject
+{
+    public List<Object> objectList;
+
+    public List<FloatData> FloatDatas;
+
+    public List<GameObject> gameObjects;
+
+    public void FindObjectType(Object obj)
+    {
+        foreach (var currentObj in objectList)
+        {
+            if (currentObj == obj)
+            {
+                //Do Work-Instanciate, adding object, etc;
+            }
+        }
+    }
+
+    public void AddToList(Object obj)
+    {
+        objectList.Add(obj);
+    }
+
+    public void RemoveFromList(Object obj)
+    {
+        foreach (var currentObj in objectList)
+        {
+            if (currentObj == obj)
+            {
+                objectList.Remove(obj);
+            }
+        }
+    }
+}
